738d81b695ad20204add379ba878849a96fd6a6a
Lorenzo Vancoillie <lorenzo@xamsservices.be>
2020-08-29 21:05:49 +0200

    Added toast on blog delete Added required annotation on BlogDTO object

--- FooBlog.Core/DTO/BlogDTO.cs
+++ FooBlog.Core/DTO/BlogDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -8,9 +9,12 @@ namespace FooBlog.Core.DTO
     public class BlogDTO
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
         public DateTime CreatedOn { get; set; }
+        [Required]
         public string Post { get; set; }
+        [Required]
         public string Description { get; set; }
         public string Image { get; set; }
     }
